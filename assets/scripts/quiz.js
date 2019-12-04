656769a716319b44765e77347f2050a66567f785
Shamil <aslanov.shm@gmail.com>
2019-12-04 19:01:28 +0300

    upd

--- assets/scripts/quiz.js
+++ assets/scripts/quiz.js
@@ -1,7 +1,113 @@
-function nextStep(btn, step) {
+// function nextStep(btn, step, items) {
+//   btn.addEventListener("click", function(e) {
+//     e.preventDefault()
+//     step.classList.add("quiz__step--active")
+//   })
+// }
+
+let i = 0;
+let index1 = 0;
+
+function start(btn, step, items) {
+  items.forEach(item => {
+    item.addEventListener('click', function(){
+
+      if(!item.classList.contains('checked')) {
+        i += 1;
+        item.classList.add('checked')
+        console.log('Элемент выбран')
+      } else {
+        i -= 1;
+        item.classList.remove('checked')
+        console.log('"Элемент не выбран')
+        btn.setAttribute("disabled", "disabled");
+      }
+
+      if(i > 0) {
+        btn.removeAttribute("disabled");
+      }
+    })
+
+    btn.addEventListener("click", function(e) {
+      e.preventDefault()
+      step.classList.add("quiz__step--active")
+    })
+  })
+}
+
+function nextStep4(btn, step, item) {
+  item.addEventListener('input', function(){
+
+    if(item.value !== '') {
+      console.log('Элемент выбран')
+      return btn.removeAttribute("disabled");
+    } else {
+      console.log('"Элемент не выбран')
+      btn.setAttribute("disabled", "disabled");
+    }
+  })
+
   btn.addEventListener("click", function(e) {
     e.preventDefault()
     step.classList.add("quiz__step--active")
+  })
+}
+
+function nextStep(btn, step, items) {
+  items.forEach(item => {
+    item.addEventListener('click', function(){
+
+      if(item) {
+        console.log('Элемент выбран')
+        btn.removeAttribute("disabled");
+
+        if(item.classList.contains('screen1')) {
+          index1 = 1;
+        }
+
+        if(item.classList.contains('screen2')) {
+          index = 2;
+          console.log('Второй экран')
+        }
+
+        if(item.classList.contains('screen3')) {
+          index = 3;
+          console.log('Третий экран')
+        }
+
+        if(items == options7) {
+          console.log('Это последний экран!')
+          showScreen(btn)
+        }
+
+      } else {
+        console.log('"Элемент не выбран')
+        btn.setAttribute("disabled", "disabled");
+      }
+    })
+
+    btn.addEventListener("click", function(e) {
+      e.preventDefault()
+      step.classList.add("quiz__step--active")
+    })
+  })
+}
+
+function showScreen (btn) {
+  btn.addEventListener("click", function() {
+    btn.removeAttribute("disabled");
+
+    if (index1 == 1) {
+      return screen1.classList.add('quiz__step--active')
+    }
+
+    if (index == 2) {
+      screen2.classList.add('quiz__step--active')
+    }
+
+    if (index == 3) {
+      screen3.classList.add('quiz__step--active')
+    }
   })
 }
 
@@ -12,48 +118,24 @@ function prevStep(btn, step) {
   })
 }
 
-function choises(items, step) {
-  items.forEach(item => {
-    item.addEventListener('click', function(){
-
-      if(item.classList.contains('screen1')) {
-        screen1.classList.add('quiz__step--active')
-      }
-
-      if(item.classList.contains('screen2')) {
-        index = 1;
-        console.log(index)
-      }
-
-      if(item.classList.contains('screen3')) {
-        index = 2;
-        console.log(index)
-      }
-
-      if(items == options7) {
-
-        if (index == 1) {
-          screen2.classList.add('quiz__step--active')
-        }
-
-        if (index == 2) {
-          screen3.classList.add('quiz__step--active')
-        }
-      }
-
-      step.classList.add('quiz__step--active')
-    })
-  })
-}
-
 let index;
 
 const btnStep1 = document.querySelector(".quiz__btn--step1");
 const btnStep2 = document.querySelectorAll(".quiz__btn--step2");
-const btnStep3 = document.querySelector(".quiz__btn--step3");
-const btnStep4 = document.querySelector(".quiz__btn--step4");
+const btnStep3 = document.querySelectorAll(".quiz__btn--step3");
+const btnStep4 = document.querySelectorAll(".quiz__btn--step4");
 const btnStep5 = document.querySelectorAll(".quiz__btn--step5");
-const btnStep6 = document.querySelector(".quiz__btn--step6");
+const btnStep6 = document.querySelectorAll(".quiz__btn--step6");
+const btnStep7 = document.querySelectorAll(".quiz__btn--step7");
+const btnResult = document.querySelector(".quiz__btn--result");
+
+const options1 = document.querySelectorAll(".quiz__select--1");
+const options2 = document.querySelectorAll(".quiz__select--2");
+const options3 = document.querySelectorAll(".quiz__select--3");
+const options4 = document.querySelector(".quiz__select--4");
+const options5 = document.querySelectorAll(".quiz__select--5");
+const options6 = document.querySelectorAll(".quiz__select--6");
+const options7 = document.querySelectorAll(".quiz__select--7");
 
 const step2 = document.querySelector(".quiz__step--2");
 const step3 = document.querySelector(".quiz__step--3");
@@ -66,34 +148,32 @@ const screen1 = document.querySelector(".quiz__screen1");
 const screen2 = document.querySelector(".quiz__screen2");
 const screen3 = document.querySelector(".quiz__screen3");
 
-nextStep(btnStep2[0], step2);
+start(btnStep2[0], step2, options1);
 prevStep(btnStep1, step2);
+
+nextStep(btnStep3[0], step3, options2);
 prevStep(btnStep2[1], step3);
-prevStep(btnStep3, step4);
-prevStep(btnStep4, step5);
-nextStep(btnStep5[0], step5);
+
+nextStep(btnStep4[0], step4, options3);
+prevStep(btnStep3[1], step4);
+
+nextStep4(btnStep5[0], step5, options4);
+prevStep(btnStep4[1], step5);
+
+nextStep(btnStep6[0], step6, options5);
 prevStep(btnStep5[1], step6);
-prevStep(btnStep6, step7);
 
-const options2 = document.querySelectorAll(".quiz__select--2")
-const options3 = document.querySelectorAll(".quiz__select--3")
-const options4 = document.querySelectorAll(".quiz__select--4")
-const options5 = document.querySelectorAll(".quiz__select--5")
-const options6 = document.querySelectorAll(".quiz__select--6")
-const options7 = document.querySelectorAll(".quiz__select--7")
+nextStep(btnStep7[0], step7, options6);
+prevStep(btnStep6[1], step7);
 
-choises(options2, step3)
-choises(options3, step4)
-choises(options4, step5)
-choises(options5, step6)
-choises(options6, step7)
-
-choises(options7, screen2)
+nextStep(btnResult, step7, options7);
 
 const quizWrap = document.querySelector(".quiz-wrap");
-const quizClose = document.querySelector(".quiz__close");
+const quizClose = document.querySelectorAll(".quiz__close");
 
-quizClose.addEventListener("click", function(event) {
-  event.preventDefault();
-  console.log(11)
+quizClose.forEach(item => {
+  item.addEventListener('click', function(){
+    event.preventDefault();
+    quizWrap.classList.add("quiz--hide")
+  })
 })
